9ac1f0d567675d96dbc69f12fc13686fc7e31963
Mostafa Hassanein <me.cssign@gmail.com>
2025-02-01 08:39:45 +0200

    fix(test): test only the MyLocalBookStore channel

--- src/test/java/com/cd/acceptance/examples/ExampleBookStoreAcceptanceTest.java
+++ src/test/java/com/cd/acceptance/examples/ExampleBookStoreAcceptanceTest.java
@@ -5,8 +5,6 @@ import com.cd.acceptance.dsl.Dsl;
 import org.junit.Before;
 import org.junit.Test;
 
-import static com.cd.acceptance.dsl.Channels.Amazon;
-import static com.cd.acceptance.dsl.Channels.BookDepository;
 import static com.cd.acceptance.dsl.Channels.MyLocalBookStore;
 
 /**
@@ -15,7 +13,7 @@ import static com.cd.acceptance.dsl.Channels.MyLocalBookStore;
 public class ExampleBookStoreAcceptanceTest extends Dsl
 {
     @Test
-    @Channel({Amazon, BookDepository, MyLocalBookStore})
+    @Channel({MyLocalBookStore})
     public void shouldAddBookToShoppingBasket() throws Exception
     {
         shopping.searchForBook("title: Continuous Delivery");
